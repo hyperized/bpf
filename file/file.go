33e92870af2bb00315dfad0344b485a2f1f65e28
Gerben Geijteman <gerben@hyperized.net>
2020-05-18 11:07:09 +0200

    Fix linter observations

--- file/file.go
+++ file/file.go
@@ -2,22 +2,31 @@ package file
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 )
 
-const permissions = 0666
-const logPrefix = "github.com/hyperized/bpf/files"
-const pattern = "/dev/bpf*"
-const emptyPath = ""
-const using = "using"
-const closing = "closing"
-const noAccess = " cannot be accessed, skipping"
-const noDescriptor = " doesn't have a valid file descriptor"
-const noDevice = "unable to find available bpf device"
-const twoLog = "%s: %s"
-const threeLog = "%s: %s %s"
+const (
+	permissions  = 0666
+	logPrefix    = "github.com/hyperized/bpf/files"
+	pattern      = "/dev/bpf*"
+	emptyPath    = ""
+	using        = "using"
+	closing      = "closing"
+	noAccess     = " cannot be accessed, skipping"
+	noDescriptor = " doesn't have a valid file descriptor"
+	noDevice     = "unable to find available bpf device"
+	twoLog       = "%s: %s"
+	threeLog     = "%s: %s %s"
+)
+
+var (
+	ErrNoAccess     = errors.New(noAccess)
+	ErrNoDescriptor = errors.New(noDescriptor)
+	ErrNoDevice     = errors.New(noDevice)
+)
 
 type File interface {
 	File() *os.File
@@ -39,7 +48,7 @@ type bpfDevice struct {
 	logging        bool
 }
 
-// Public interface methods
+// Public interface methods.
 func (b *bpfDevice) File() *os.File {
 	return b.file
 }
@@ -56,10 +65,11 @@ func (b *bpfDevice) Close() error {
 	if b.hasLogging() {
 		log.Printf("%s: %s %s", logPrefix, closing, b.path)
 	}
+
 	return b.file.Close()
 }
 
-// Private interface methods
+// Private interface methods.
 func (b *bpfDevice) getFile() (*os.File, error) {
 	return os.OpenFile(b.path, os.O_RDWR, permissions)
 }
@@ -69,7 +79,8 @@ func (b *bpfDevice) canBeOpened() (bool, error) {
 		b.file = file
 		return true, nil
 	}
-	return false, errors.New(b.path + noAccess)
+
+	return false, fmt.Errorf("%s %w", b.path, ErrNoAccess)
 }
 
 func (b *bpfDevice) getFileDescriptor() uintptr {
@@ -82,16 +93,18 @@ func (b *bpfDevice) canObtainFileDescriptor() (bool, error) {
 		b.fileDescriptor = fd
 		return true, nil
 	}
-	return false, errors.New(b.path + noDescriptor)
+
+	return false, fmt.Errorf("%s %w", b.path, ErrNoDescriptor)
 }
 
 func (b *bpfDevice) hasLogging() bool {
 	return b.logging
 }
 
-// public methods
+// Public methods.
 func GetBpfDevice(logging bool) (file File, err error) {
 	var paths []string
+
 	file = newBpfDevice()
 	paths, err = listBpfDevices()
 
@@ -100,26 +113,32 @@ func GetBpfDevice(logging bool) (file File, err error) {
 			logging: logging,
 			path:    path,
 		}
+
 		if ok, err := file.canBeOpened(); !ok {
 			myLog(err, file)
 			continue
 		}
+
 		if ok, err := file.canObtainFileDescriptor(); !ok {
 			myLog(err, file)
 			continue
 		}
+
 		break
 	}
+
 	if file.Path() == emptyPath {
-		err = errors.New(noDevice)
+		err = ErrNoDevice
 	}
+
 	if file.hasLogging() {
 		log.Printf(threeLog, logPrefix, using, file.Path())
 	}
-	return
+
+	return file, err
 }
 
-// private methods
+// Private methods.
 func newBpfDevice() *bpfDevice {
 	return &bpfDevice{}
 }
